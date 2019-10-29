d57ec201e61fdc3d7f12445d44a1fcae51742c0f
jlal92 <54960889+jlal92@users.noreply.github.com>
2019-10-29 16:10:54 -0700

    Add files via upload

---
+++ min_max_difference.cs
@@ -0,0 +1,18 @@
+namespace min_max_difference
+{
+    class Program
+    {
+        static void main ()
+        {
+            int min_max ();
+            list min_max = new list min_max[];
+
+             var new list = [15, 22, 84, 14, 88, 23];
+
+             var max = list.max();
+             var min = list.min();
+             var value = list.max() - list.min();
+        }
+    }
+
+}
